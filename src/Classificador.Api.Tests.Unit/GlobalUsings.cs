84ae1c2fdc5212ff268d87c789054a70e815ccd6
Igor <Igor.almeidadasilva13@gmail.com>
2024-08-15 16:44:01 -0300

    update: Organizing global usings of the UnitTests project

--- src/Classificador.Api.Tests.Unit/GlobalUsings.cs
+++ src/Classificador.Api.Tests.Unit/GlobalUsings.cs
@@ -31,3 +31,4 @@ global using Classificador.Api.Application.Commands.SendEmailToContact;
 global using Classificador.Api.Application.Commands.CreateClassification;
 global using Classificador.Api.Application.Dtos;
 global using Classificador.Api.Application.Queries.GetCategories;
+global using Classificador.Api.Application.Queries.GetPrescribingInformation;
