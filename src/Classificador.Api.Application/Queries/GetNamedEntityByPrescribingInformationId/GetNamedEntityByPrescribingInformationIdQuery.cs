5a87ffe2a39ba1d760070991f6bb6899393c382c
Igor <Igor.almeidadasilva13@gmail.com>
2024-08-18 17:52:44 -0300

    update: Guid IdUser

--- src/Classificador.Api.Application/Queries/GetNamedEntityByPrescribingInformationId/GetNamedEntityByPrescribingInformationIdQuery.cs
+++ src/Classificador.Api.Application/Queries/GetNamedEntityByPrescribingInformationId/GetNamedEntityByPrescribingInformationIdQuery.cs
@@ -3,10 +3,12 @@ namespace Classificador.Api.Application.Queries.GetNamedEntityByPrescribingInfor
 public sealed record GetNamedEntityByPrescribingInformationIdQuery : IQuery<Result>
 {
     public Guid IdPrescribingInformation { get; set; }
+    public Guid IdUser { get; set; }
 
-    public GetNamedEntityByPrescribingInformationIdQuery(string idPrescribingInformation)
+    public GetNamedEntityByPrescribingInformationIdQuery(string idPrescribingInformation, string idUser)
     {
         IdPrescribingInformation = new Guid(idPrescribingInformation);
+        IdUser = new Guid(idUser);
     }
-    
+
 }
