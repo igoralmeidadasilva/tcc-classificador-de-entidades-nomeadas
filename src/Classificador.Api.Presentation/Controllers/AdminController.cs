3d31c5f452d40c37ae82f7561ea0b7d31ee939fc
Igor <Igor.almeidadasilva13@gmail.com>
2024-08-18 13:14:50 -0300

    implementation: action result AdminPanel

--- src/Classificador.Api.Presentation/Controllers/AdminController.cs
+++ src/Classificador.Api.Presentation/Controllers/AdminController.cs
@@ -4,6 +4,9 @@ namespace Classificador.Api.Presentation.Controllers;
 [Authorize(Roles = nameof(UserRole.Admin))]
 public sealed class AdminController(ILogger<AdminController> logger, IMediator mediator) : WebController<AdminController>(logger, mediator)
 {
-    
-
+    [HttpGet(nameof(AdminPanel))]
+    public IActionResult AdminPanel()
+    {
+        return View();
+    }
 }
