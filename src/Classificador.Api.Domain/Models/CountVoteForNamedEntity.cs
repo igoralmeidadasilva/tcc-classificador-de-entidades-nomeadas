82b1fa94d4e290f88afd021fe1721b16a25b0cc3
Igor <Igor.almeidadasilva13@gmail.com>
2024-09-29 15:06:40 -0300

    feat(DownloadClassificationSpacyModel): Updating the GetMostVotedEntityByPrescribingInformation method to suit the new classification

--- src/Classificador.Api.Domain/Models/CountVoteForNamedEntity.cs
+++ src/Classificador.Api.Domain/Models/CountVoteForNamedEntity.cs
@@ -5,4 +5,6 @@ public sealed record CountVoteForNamedEntity
     public int Votes { get; init; }
     public string? Entity { get; init; }
     public string? Category { get; init; }
+    public int Start { get; init; }
+    public int End { get; init; }
 }
--- src/Classificador.Api.Presentation/Models/PrecribingInformationClassificationsViewModel.cs
+++ src/Classificador.Api.Presentation/Models/PrecribingInformationClassificationsViewModel.cs
@@ -3,5 +3,6 @@ namespace Classificador.Api.Presentation.Models;
 public sealed record class PrecribingInformationClassificationsViewModel
 {
     public string? NamePrescribingInformation { get; set; }
-    public List<CountVoteForNamedEntity> Classifications { get; set; } = [];
+    public Guid IdPrescribingInformation { get; set; }
+    public List<CountVoteForNamedEntity>? Classifications { get; set; }  = [];
 }
